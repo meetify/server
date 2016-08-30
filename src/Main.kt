591087743217e3103e1978f2d84b469ff3216365
Misipuk <tretyakov@dlit.dp.ua>
2016-08-30 22:36:24 +0300

    Test #2

--- src/Main.kt
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Dima on 30.08.2016.
- */
-
-fun main(args: Array<String>) {
-
-}
---
+++ src/src/Main.kt
@@ -0,0 +1,13 @@
+package src
+
+/**
+ * Created by Maks on 30.08.2016.
+ */
+
+class Main {
+
+}
+
+fun main(args:Array<String>) {
+    println("hi?")
+}
