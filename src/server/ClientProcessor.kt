d302d3e915ed0b82f98a0413ae49670e01c12527
kr3v <d.baynak@protonmail.ch>
2016-08-31 01:58:15 +0300

    Server addition (basic implementation, added TODOs)

---
+++ src/server/ClientProcessor.kt
@@ -0,0 +1,14 @@
+package server
+
+import java.net.Socket
+
+/**
+ * Created by Dima on 31.08.2016.
+ * Client processor is class which used to response on client's requests.
+ * TODO: implement basic parts of this class
+ */
+class ClientProcessor(var socket: Socket) : Runnable {
+    override fun run() {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ src/server/Server.kt
@@ -0,0 +1,70 @@
+package server
+
+import java.net.BindException
+import java.net.ServerSocket
+import java.util.logging.Logger
+
+/**
+ * Created by Dima on 31.08.2016.
+ * Server main class. If
+ */
+class Server private constructor(
+        var port: Int,
+        var socket: ServerSocket,
+        var logger: Logger,
+        var running: Boolean) : Runnable {
+
+    constructor(port: Int) : this(
+            port,
+            { ->
+                try {
+                    ServerSocket(port)
+                } catch(e: BindException) {
+                    Logger.getAnonymousLogger().warning("port $port is already used")
+                    throw IllegalArgumentException("port $port is already used")
+                }
+            }(),
+            Logger.getAnonymousLogger(),
+            false) {
+
+    }
+
+    override fun run() {
+        synchronized(running) {
+            if (!running) {
+                running = true
+            } else {
+                logger.warning("this server running not in this Server.run, finishing, ${toString()}")
+                return
+            }
+        }
+
+        if (socket.isClosed) {
+            logger.warning("socket is closed, trying to open again on $port, ${toString()}")
+            try {
+                socket = ServerSocket(port)
+            } catch(e: BindException) {
+                /**
+                 * TODO: info about BindException in Server.run, if 'running' condition isn't enough
+                 */
+                logger.warning("port $port is already used, finishing server, ${toString()}")
+                running = false
+                return
+            }
+        }
+
+        do {
+            val socketAccept = socket.accept()
+            if (socketAccept != null) {
+                Thread(ClientProcessor(socketAccept)).start()
+            }
+
+        } while (socketAccept != null)
+
+        if (!socket.isClosed) {
+            socket.close()
+        }
+        running = false
+    }
+
+}
