6205f28654449614810ebd94063333b2888a6e32
kr3v <d.baynak@protonmail.ch>
2016-09-06 21:28:59 +0300

    SocketWorker now use submit only once per socket

--- src/main/kotlin/server/SocketWorker.kt
+++ src/main/kotlin/server/SocketWorker.kt
@@ -12,24 +12,29 @@ object SocketWorker : Runnable {
     val executorService = Executors.newCachedThreadPool()!!
 
     override fun run() {
-        while (!Server.socketServer!!.isClosed) {
-//            logger.warning("again")
+        while (!Server.socketServer.isClosed) {
             socketList.forEach(fun(socket: SimpleSocket) {
                 if (!socket.used && socket.isClosed()) {
                     logger.warning("remove")
                     socketList.remove(socket)
-                } else if (socket.available()) {
-                    synchronized(socket.used) {
-                        logger.warning("available")
-                        socket.used = true
-                        executorService.submit {
-                            ClientProcessor(socket).run()
-                            socket.used = false
-                        }
+                    return
+                }
+                synchronized(socket.used) {
+                    if (socket.used) {
+                        return
                     }
+                    socket.used = true
+                }
+
+                if (socket.available() && socket.used) {
+                    executorService.submit {
+                        ClientProcessor(socket).run()
+                        socket.used = false
+                    }
+                } else {
+                    socket.used = false
                 }
             })
         }
     }
-
 }
